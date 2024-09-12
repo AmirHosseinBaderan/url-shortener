09f91b07a36058270b96bc9621de3c4eac39f17c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-09-12 18:55:10 +0330

    create : url details

---
+++ ShortenerUrl/Shortner/Grain/Url/UrlDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Shortner.Grain;
+
+[GenerateSerializer, Alias(nameof(UrlDetails))]
+public class UrlDetails
+{
+    [Id(0)]
+    public string LongUrl { get; set; } = null!;
+
+    [Id(1)]
+    public string ShortCode { get; set; } = null!;
+}
